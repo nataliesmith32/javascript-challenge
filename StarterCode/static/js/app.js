6340c75861018266c6914c4ed2ff8f9e61bed22b
nataliesmith32 <natalielsmith32@gmail.com>
2020-12-21 09:42:37 -0700

    clean files

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,7 +3,7 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 
-//loop through data and print the page
+//loop through data and print the initial table --- needed to be defined inside a function!
 function buildTable(data) {
     tbody.html("");
     data.forEach((UFO_data) => {
@@ -27,33 +27,6 @@ function handleClick(){
     }
     buildTable(filterData);
 }
-
 d3.selectAll("#filter-btn").on('click', handleClick);
-
 buildTable(tableData);
-
-
-
-// // Complete the event handler function for the form
-// function runEnter() {
-//     var button = d3.select("#filter-btn");
-//     var form = d3.select("#form");
-
-//     button.on("click", runEnter);
-//     form.on("submit",runEnter);
-
-//   // Prevent the page from refreshing
-//     d3.event.preventDefault();
-  
-//   //define the user input
-//     var inputElement = d3.select("#datetime");
-//     var inputValue = inputElement.property("value");
-
-//   if(inputElement) {
-//     filteredData = tableData.filter((row) => row.datetime === inputValue); 
-//     button.on("click", runEnter);
-//     buildTable(filteredData);
-//   }
-
-// }
 
